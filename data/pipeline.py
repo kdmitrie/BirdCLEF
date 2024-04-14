7559b40d819e49cfc889218b692b59e94e3d73a9
kdmitrie <presentatio@mail.ru>
2024-04-14 15:23:30 +0300

    Add ExpandDims processor

--- data/pipeline.py
+++ data/pipeline.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import numpy as np
 from .loader import DataItem, DataProcessor
 
 
@@ -11,4 +12,18 @@ class DataPipeline(DataProcessor):
     def process(self, item: DataItem) -> DataItem:
         for proc in self.pipeline:
             item = proc(item)
+        return item
+
+
+@dataclass
+class ExpandDims(DataProcessor):
+    """Adds new dimensions to the DataItem"""
+
+    field: str
+    dims: int = 0
+
+    def process(self, item: DataItem) -> DataItem:
+        data = getattr(item, self.field)
+        data = np.expand_dims(data, self.dims)
+        setattr(item, self.field, data)
         return item
