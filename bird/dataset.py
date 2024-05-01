1c9bee19ea29e354f217fa4a5b90b92ad20dff56
kdmitrie <presentatio@mail.ru>
2024-05-01 22:27:12 +0300

    Fix errors with CFG instead of cfg in dataset and incorrect length of data in model

--- bird/dataset.py
+++ bird/dataset.py
@@ -76,7 +76,7 @@ class BirdDataset(torch.utils.data.Dataset):
         else:
             data, _ = librosa.load(f'/kaggle/input/birdclef-2024/train_audio/{item.filename}', sr=self.cfg.FS)
             data = data[:self.cfg.FS * self.cfg.max_duration]
-            pad = np.ceil(len(data) / (self.cfg.FS * self.CFG.STEP)) * self.cfg.FS * self.CFG.STEP - len(data)
+            pad = np.ceil(len(data) / (self.cfg.FS * self.cfg.STEP)) * self.cfg.FS * self.cfg.STEP - len(data)
             if pad > 0:
                 data = np.pad(data, (0, pad))
 
